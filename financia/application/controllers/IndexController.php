7c2a70d12648d9ce52a5167291b5cef876c6a3c5
George Mathew K <georgemathewk@yahoo.com>
2011-05-19 20:44:06 +0530

    Edited the comment2

--- financia/application/controllers/IndexController.php
+++ financia/application/controllers/IndexController.php
@@ -5,7 +5,7 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        /* Initialize action controller here*/
     }
 
     public function indexAction()
