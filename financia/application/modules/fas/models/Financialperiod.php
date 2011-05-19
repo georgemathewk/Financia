5a2476fb3f5b2e442c6c5ebd465fb6852dc4d3aa
George Mathew K <georgemathewk@yahoo.com>
2011-05-19 06:45:07 +0530

    Model class Fas_Model_Financialperid is renamed to Fas_Model_FinancialPeriod. File name is also change. Properties id,code, name,fdate, tdate and remarks are added. Constructor is added. getall function is added

---
+++ financia/application/modules/fas/models/Financialperiod.php
@@ -0,0 +1,40 @@
+<?php
+
+class Fas_Model_Financialperiod
+{
+	private $id;
+	private $code;
+	private $name;
+	private $fdate;
+	private $tdate;
+	private $remarks;
+	
+	public function __construct($id,$code,$name,$fdate,$tdate,$remarks){
+		$this->id	 	= $id;
+		$this->code	 	= $code;
+		$this->name	 	= $name;
+		$this->fdate	= $fdate;
+		$this->tdate	= $tdate;
+		$this->remarks	= $remarks;
+	}
+	
+	public function getall(){
+		$financialperiod = new Fas_Model_DbTable_Financialperiod();
+		$rowset = $financialperiod->fetchAll();
+		$xml	= "<?xml version='1.0' encoding='utf-8' ?>";
+		$xml	.= "<rows>";
+			
+		foreach($rowset as $row){
+			$xml	.= "<row>";
+			$xml	.= "<cell>".$row->id."</cell>";
+			$xml	.= "<cell>".$row->code."</cell>";
+			$xml	.= "<cell>".$row->fdate."</cell>";
+			$xml	.= "<cell>".$row->remarks."</cell>";
+			$xml	.= "</row>";	
+		}
+		$xml	.= "</rows>";
+		return $xml;
+	}
+
+}
+
