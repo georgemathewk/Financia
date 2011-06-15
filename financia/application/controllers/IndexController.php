ff6bf26d4f3b8aedd9670678d5f56c2b1b79158e
George Mathew K <georgemathewk@yahoo.com>
2011-06-15 07:49:54 +0530

    Login Screen is added

--- financia/application/controllers/IndexController.php
+++ financia/application/controllers/IndexController.php
@@ -11,8 +11,18 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $this->_helper->layout()->disableLayout();
+    }
+
+    public function homeAction()
+    {
+        // action body
     }
 
 
 }
+
+
+
+
 
