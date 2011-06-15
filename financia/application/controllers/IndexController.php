a99d055a26b90f75fb8b7e21810191f2d91f8395
George Mathew K <georgemathewk@yahoo.com>
2011-06-15 21:36:38 +0530

    Zend_Auth is implemented for user authentication

--- financia/application/controllers/IndexController.php
+++ financia/application/controllers/IndexController.php
@@ -18,8 +18,28 @@ class IndexController extends Zend_Controller_Action
     {
         // action body
     }
-
-
+    
+    public function loginAction() {
+    	$dbAdapter = Zend_Db_Table::getDefaultAdapter();
+        $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+        
+        $authAdapter->setTableName('user')
+            ->setIdentityColumn('user')
+            ->setCredentialColumn('passwd');
+            //->setCredentialTreatment('SHA1(CONCAT(?,salt))');
+       
+       $authAdapter->setIdentity($_POST['name']); 
+       $authAdapter->setCredential($_POST['password']);
+        
+       $auth = Zend_Auth::getInstance();
+       $result = $auth->authenticate($authAdapter);
+       if ($result->isValid()) {
+            $user = $authAdapter->getResultRowObject();
+            $auth->getStorage()->write($user);
+            $this->_helper->redirector('home','index');
+       }
+       $this->_helper->redirector('index','index');         
+    }
 }
 
 
