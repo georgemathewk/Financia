018144b1ead48566d60c6a80ffc6b9aff0f2a58e
George Mathew K <georgemathewk@yahoo.com>
2011-07-17 12:15:34 +0530

    Tables for storing voucher information is added to the project

--- financia/docs/db.sql
+++ financia/docs/db.sql
@@ -41,3 +41,50 @@ create table user (
 
 
 
+create table transaction_mode(
+	id	int	auto_increment primary key,
+	code	varchar(100),
+	name	varchar(100),
+	remarks text
+)engine=innodb;
+
+create table voucher_type (
+	id	int	auto_increment primary key,
+	code	varchar(100)	unique,
+	name	varchar(100),
+	remarks	text
+)engine=innodb;
+
+create table voucher(
+	id		int	auto_increment	primary key,
+	voucher_no	varchar(100),
+	voucher_date	timestamp	not null default now(),
+	voucher_type_id	int,
+	constraint uk unique(voucher_no,voucher_date),
+	constraint v_fk1 foreign key(voucher_type_id) references voucher_type(id)
+)engine=innodb;
+
+
+create table voucher_details(
+	id		int	auto_increment	primary key,
+	voucher_id	int,
+	accounthead_id	int,
+	debit_credit 	int,
+	amount		decimal(30,2),
+	constraint vd_fk1 foreign key(voucher_id) references voucher(id),
+	constraint vd_fk2 foreign key(accounthead_id) references accounthead(id)
+)engine=innodb;
+
+
+create table voucher_transaction_mode(
+	id			int	auto_increment primary key,
+	voucher_id		int,
+	transaction_mode_id	int,
+	amount			decimal(30,2),
+	constraint vtm_fk1 foreign key(voucher_id) references voucher(id),
+	constraint vtm_fk2 foreign key(transaction_mode_id) reference transaction_mode(id)
+
+)engine=innodb;
+
+
+
